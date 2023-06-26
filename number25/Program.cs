5dba9c03e443d8432a19a86bfb2e20c8f2b4608d
Liudmila <yaveryvz@mail.ru>
2023-06-26 17:55:55 +0300

    add number5,27

---
+++ number25/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите цикл, который принимает на вход 2 числа А и В и возводит число А\
+// в натуральную степень В
+// 3, 5 -> 243
+// 2, 4 -> 16
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+int Power(int powerBase,int exponent)
+{
+  int power = 1;
+  for(int i = 0; i < exponent; i++)
+ {
+    power*=powerBase;
+ }
+return power;
+}
+
+bool ValidateExponent(int exponent)
+{
+ if (exponent <0)
+ {
+    System.Console.WriteLine("Введено некорректное число В, введите натуальное число В: ");
+    return false;
+ }
+return true;
+}
+
+int powerBase = Prompt("Введите число А: ");
+int exponent= Prompt("Введите число В: ");
+if (ValidateExponent(exponent))
+{
+    System.Console.WriteLine($"Число {powerBase} в степени {exponent} равно {Power(powerBase,exponent)}");
+}
---
+++ number27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму
+// цифр в числе
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+int SummaNum(int number)
+{
+  int result = 0 ;
+  while(number > 0)
+ {
+    result += number%10;
+    number = number/10;
+ }
+return result;
+}
+
+int number = Prompt("Введите число: ");
+ System.Console.WriteLine($"{number} -> {SummaNum(number)}");
